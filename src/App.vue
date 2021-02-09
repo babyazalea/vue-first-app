5b646520e58a44b1052f9ba61f17b50271c08979
babyazalea <usun12@hotmail.com>
2021-02-09 17:23:37 +0900

    building App.vue

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li :key="friend.id" v-for="friend of friends">
+        {{ friend.name }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "em",
+          name: "eunmin",
+          phone: "101010",
+          email: "test@test.com",
+        },
+        {
+          id: "em2",
+          name: "eunmin2",
+          phone: "1010102",
+          email: "test2@test2.com",
+        },
+      ],
+    };
+  },
+};
+</script>
